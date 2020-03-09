1b8c8c78eba48f15e2f1f6e9b42bfc0a6142ce0a
Emmanuel <emmanuel.ennin.2022@student.lhschools.org>
2020-03-09 13:09:34 -0400

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,26 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        testOddness();
+    }
+
+
+    public static void testOddness() {
+        System.out.println("testOddness");
+
+        int pNumber=10;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Type a positive number");
+        pNumber= keyboard.nextInt();
+
+
+        System.out.println("Number is odd "+ (pNumber % 2 !=0));
+
     }
 }
