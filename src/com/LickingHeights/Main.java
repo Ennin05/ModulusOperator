142a38ace6419551beae6ae5e66a6e0bca5ffe68
Emmanuel <emmanuel.ennin.2022@student.lhschools.org>
2020-03-11 14:11:22 -0400

    lastDigitMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,41 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {  keepContained();
+    public static void main(String[] args) { LastDigit();
     }
 
 
-    public static void keepContained() {
-        //Want to keep number contained
-        int number = 0;
+    public static void LastDigit() {
+        Scanner
+        Keyboard = new Scanner( System.in);
+        int number;
 
-        System.out.println("number contained is: " + (number%5));
+        System.out.println("Type a 4-digit positive number");
+        number = Keyboard.nextInt();
 
-        System.out.println(number%5);
+        System.out.println("The Last Digit is: " + (number%10));
 
-        number = 1;
-        System.out.println(number%5);
-
-        number = 2;
-        System.out.println(number%5);
-
-        number = 3;
-        System.out.println(number%5);
-
-        number = 4;
-        System.out.println(number%5);
-
-        number = 5;
-        System.out.println(number%5);
-
-        number = 6;
-        System.out.println(number%5);
-
-        number = 7;
-        System.out.println(number%5);
-
-
-
-
-    }
+       }
 }
